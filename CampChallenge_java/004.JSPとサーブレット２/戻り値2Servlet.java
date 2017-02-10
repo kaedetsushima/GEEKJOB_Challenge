f423104f5d81aae9c5a3717160e255ad37b9a9ac
津島楓 <maple.0109@gmail.com>
2017-02-10 18:29:41 +0900

    戻り値２

---
+++ CampChallenge_java/004.JSPとサーブレット２/戻り値2Servlet.java
@@ -0,0 +1,113 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.servlet.java.kaede;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.*;
+
+/**
+ * 戻り値２
+ *
+ * 戻り値としてある人物のid(数値),名前,生年月日、住所を返却するメソッドを作成し、 受け取った後はid以外の値を表示する
+ *
+ * @author i_am_kaede
+ */
+public class 戻り値2Servlet extends HttpServlet {
+
+    ArrayList<String> prof() {
+        ArrayList<String> x = new ArrayList<String>();
+        x.add("id:1");
+        x.add("津島楓");
+        x.add("１月９日");
+        x.add("山梨県");
+
+        return x;
+
+    }
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet 戻り値2Servlet</title>");
+            out.println("</head>");
+            out.println("<body>");
+
+            ArrayList<String> profile = prof();
+
+            for (String i : profile) {
+
+                if (i == profile.get(0)) {
+
+                    out.print("");
+                } else {
+                    out.print(i);
+
+                }
+            }
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
