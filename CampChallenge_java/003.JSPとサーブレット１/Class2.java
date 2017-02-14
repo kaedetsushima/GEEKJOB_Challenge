e84bf12d69b7995afaef9f76c611875ca209219d
津島楓 <maple.0109@gmail.com>
2017-02-14 11:57:42 +0900

    クラスの継承

---
+++ CampChallenge_java/003.JSPとサーブレット１/Class2.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.servlet.java.kaede;
+
+/**
+ *
+ * @author i_am_kaede
+ */
+public class Class2 extends Class1{
+
+public void clear(){
+    this.age=0;
+    this.name="";
+}    
+}
